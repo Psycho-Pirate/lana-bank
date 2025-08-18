2b74b5d2e1fe53491dd72d2121273c94afcef637
Siddharth <siddharthtiwarikreplind@gmail.com>
2025-08-18 15:40:31 +0530

    chore: cypress config changes for memory-management (#2595)
    * chore: cypress config changes
    
    * chore: skip running data stack
    
    * chore: use DATA_PIPELINE variable
    
    * chore: revert DATA_PIPELINE variable changes

--- apps/admin-panel/cypress.config.ts
+++ apps/admin-panel/cypress.config.ts
@@ -5,14 +5,14 @@ const multiplier = 10 // Browserstack local tunnel on GHA Runner can be quite sl
 export default defineConfig({
   e2e: {
     setupNodeEvents(on) {
-      on('task', {
+      on("task", {
         checkUrl(url: string) {
           return new Promise((resolve) => {
             fetch(url)
-              .then(response => resolve(response.ok))
+              .then((response) => resolve(response.ok))
               .catch(() => resolve(false))
           })
-        }
+        },
       })
       on("before:browser:launch", (browser, launchOptions) => {
         if (browser.name === "chrome") {
@@ -64,6 +64,7 @@ export default defineConfig({
     pageLoadTimeout: 60000 * multiplier,
     retries: 5,
     screenshotOnRunFailure: false,
+    experimentalMemoryManagement: true,
     video: true,
     screenshotsFolder: "cypress/manuals/screenshots",
     env: {
